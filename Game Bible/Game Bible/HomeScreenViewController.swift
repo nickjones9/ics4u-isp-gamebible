570c4f5cffc9712c16b446bd3be0f81a498f22b1
Nick Jones <nickj918@hotmail.com>
2017-03-01 18:55:24 -0500

    Game Detail View updated. Resolves #6

--- Game Bible/Game Bible/HomeScreenViewController.swift
+++ Game Bible/Game Bible/HomeScreenViewController.swift
@@ -12,7 +12,6 @@ class HomeScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 

592fe0ce9ece90c72cbb17c8aff03717ef963705
Nick Jones <nickj918@hotmail.com>
2017-03-12 14:02:07 -0400

    Updating category table view when experienced an issue with the dataSource connection from table view to view controller. #8

---
+++ Game Bible/CategoryTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CategoryTableViewController.swift
+//  Game Bible
+//
+//  Created by Nicholas Jones on 2017-03-12.
+//  Copyright © 2017 Nicholas Jones. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var categories = ["Card Games", "Dice Games", "Domino Games", "Casino Games"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Return number of rows in section
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
